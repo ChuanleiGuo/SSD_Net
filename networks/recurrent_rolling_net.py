e96489aa28a386b577401c6a5342e34f78da9c15
guo.chuanlei <chuanleiguo@gmail.com>
2017-11-22 09:45:50 +0800

    refactored the function of create rolling net

--- networks/recurrent_rolling_net.py
+++ networks/recurrent_rolling_net.py
@@ -232,37 +232,11 @@ def get_symbol_rolling_train(network,
     layers = multi_layer_feature(
         body, from_layers, num_filters, strides, pads, min_filter=min_filter)
 
-    loc_preds, cls_preds, anchor_boxes = multibox_layer(layers, \
-        num_classes, sizes=sizes, ratios=ratios, normalization=normalizations, \
-        num_channels=num_filters, clip=False, interm_layer=0, steps=steps)
-
-    tmp = mx.contrib.symbol.MultiBoxTarget(
-        *[anchor_boxes, label, cls_preds], overlap_threshold=.5, \
-        ignore_label=-1, negative_mining_ratio=3, minimum_negative_samples=0, \
-        negative_mining_thresh=.5, variances=(0.1, 0.1, 0.2, 0.2),
-        name="multibox_target")
-    loc_target = tmp[0]
-    loc_target_mask = tmp[1]
-    cls_target = tmp[2]
-
-    cls_prob = mx.symbol.SoftmaxOutput(data=cls_preds, label=cls_target, \
-        ignore_label=-1, use_ignore=True, grad_scale=1., multi_output=True, \
-        normalization='valid', name="cls_prob")
-    loc_loss_ = mx.symbol.smooth_l1(name="loc_loss_", \
-        data=loc_target_mask * (loc_preds - loc_target), scalar=1.0)
-    loc_loss = mx.symbol.MakeLoss(loc_loss_, grad_scale=1., \
-        normalization='valid', name="loc_loss")
-
-    # monitoring training status
-    cls_label = mx.symbol.MakeLoss(
-        data=cls_target, grad_scale=0, name="cls_label")
-    det = mx.contrib.symbol.MultiBoxDetection(*[cls_prob, loc_preds, anchor_boxes], \
-        name="detection", nms_threshold=nms_thresh, force_suppress=force_suppress,
-        variances=(0.1, 0.1, 0.2, 0.2), nms_topk=nms_topk)
-    det = mx.symbol.MakeLoss(data=det, grad_scale=0, name="det_out")
-
     # group output
-    out = mx.symbol.Group([cls_prob, loc_loss, cls_label, det])
+    out = add_forawrd_and_loss_for_extra(layers, label=label, num_classes=num_classes,
+        num_filters=num_filters, sizes=sizes, ratios=ratios, normalizations=normalizations,
+        steps=steps, nms_thresh=nms_thresh, force_suppress=force_suppress, nms_topk=nms_topk,
+        rolling_idx=0)
 
     outputs = [out]
 
@@ -270,38 +244,50 @@ def get_symbol_rolling_train(network,
     for roll_idx in range(1, rolling_time + 1):
         roll_layers = create_rolling_struct(layers, num_outputs=num_outputs, odd=odd,
             rolling_rate=rolling_rate, roll_idx=roll_idx, conv2=False, normalize=True)
-        loc_preds, cls_preds, anchor_boxes = multibox_layer(roll_layers, \
-        num_classes, sizes=sizes, ratios=ratios, normalization=normalizations, \
-        num_channels=num_filters, clip=False, interm_layer=0, steps=steps)
-
-        tmp = mx.contrib.symbol.MultiBoxTarget(
-        *[anchor_boxes, label, cls_preds], overlap_threshold=.5, \
-        ignore_label=-1, negative_mining_ratio=3, minimum_negative_samples=0, \
-        negative_mining_thresh=.5, variances=(0.1, 0.1, 0.2, 0.2),
-        name="multibox_target_%d" % roll_idx)
-
-        loc_target = tmp[0]
-        loc_target_mask = tmp[1]
-        cls_target = tmp[2]
-
-        cls_prob = mx.symbol.SoftmaxOutput(data=cls_preds, label=cls_target, \
-            ignore_label=-1, use_ignore=True, grad_scale=1., multi_output=True, \
-            normalization='valid', name="cls_prob_%d" % roll_idx)
-        loc_loss_ = mx.symbol.smooth_l1(name="loc_loss__%d" % roll_idx, \
-            data=loc_target_mask * (loc_preds - loc_target), scalar=1.0)
-        loc_loss = mx.symbol.MakeLoss(loc_loss_, grad_scale=1., \
-            normalization='valid', name="loc_loss_%d" % roll_idx)
-
-        # monitoring training status
-        cls_label = mx.symbol.MakeLoss(
-            data=cls_target, grad_scale=0, name="cls_label_%d" % roll_idx)
-        det = mx.contrib.symbol.MultiBoxDetection(*[cls_prob, loc_preds, anchor_boxes], \
-            name="detection_%d" % roll_idx, nms_threshold=nms_thresh, force_suppress=force_suppress,
-            variances=(0.1, 0.1, 0.2, 0.2), nms_topk=nms_topk)
-        det = mx.symbol.MakeLoss(data=det, grad_scale=0, name="det_out_%d" % roll_idx)
-
-        out = mx.symbol.Group([cls_prob, loc_loss, cls_label, det])
+        out = add_forawrd_and_loss_for_extra(roll_layers, label=label, num_classes=num_classes,
+            num_filters=num_filters, sizes=sizes, ratios=ratios, normalizations=normalizations,
+            steps=steps, nms_thresh=nms_thresh, force_suppress=force_suppress, nms_topk=nms_topk,
+            rolling_idx=roll_idx)
 
         outputs.append(out)
 
     return outputs
+
+
+def add_forawrd_and_loss_for_extra(extra_layers, label, num_classes, num_filters,
+        sizes, ratios, normalizations=-1, steps=[], nms_thresh=0.5,
+        force_suppress=False, nms_topk=400, rolling_idx=0, **kwargs):
+
+    loc_preds, cls_preds, anchor_boxes = multibox_layer(extra_layers, \
+        num_classes, sizes=sizes, ratios=ratios, normalization=normalizations, \
+        num_channels=num_filters, clip=False, interm_layer=0, steps=steps)
+
+    tmp = mx.contrib.symbol.MultiBoxTarget(
+        *[anchor_boxes, label, cls_preds], overlap_threshold=.5, \
+        ignore_label=-1, negative_mining_ratio=3, minimum_negative_samples=0, \
+        negative_mining_thresh=.5, variances=(0.1, 0.1, 0.2, 0.2),
+        name="multibox_target_%d" % rolling_idx)
+    loc_target = tmp[0]
+    loc_target_mask = tmp[1]
+    cls_target = tmp[2]
+
+    cls_prob = mx.symbol.SoftmaxOutput(data=cls_preds, label=cls_target, \
+        ignore_label=-1, use_ignore=True, grad_scale=1., multi_output=True, \
+        normalization='valid', name="cls_prob_%d" % rolling_idx)
+    loc_loss_ = mx.symbol.smooth_l1(name="loc_loss__%d" % rolling_idx, \
+        data=loc_target_mask * (loc_preds - loc_target), scalar=1.0)
+    loc_loss = mx.symbol.MakeLoss(loc_loss_, grad_scale=1., \
+        normalization='valid', name="loc_loss_%d" % rolling_idx)
+
+    # monitoring training status
+    cls_label = mx.symbol.MakeLoss(
+        data=cls_target, grad_scale=0, name="cls_label_%d" % rolling_idx)
+    det = mx.contrib.symbol.MultiBoxDetection(*[cls_prob, loc_preds, anchor_boxes], \
+        name="detection_%d" % rolling_idx, nms_threshold=nms_thresh, force_suppress=force_suppress,
+        variances=(0.1, 0.1, 0.2, 0.2), nms_topk=nms_topk)
+    det = mx.symbol.MakeLoss(data=det, grad_scale=0, name="det_out_%d" % rolling_idx)
+
+    # group output
+    out = mx.symbol.Group([cls_prob, loc_loss, cls_label, det])
+
+    return out
