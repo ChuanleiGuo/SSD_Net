9f8a2e0b4e7287ba37a809ae20e9e0883c73e54f
ChuanleiGuo <chuanleiguo@gmail.com>
2017-12-31 09:55:34 +0800

    updated how kitti can be generated

--- tools/prepare_kitti.sh
+++ tools/prepare_kitti.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-python $DIR/prepare_dataset.py --dataset kitti --set taining --target $DIR/../data/train.lst --shuffle True --root $DIR/../data/kitti
-python $DIR/prepare_dataset.py --dataset kitti --set testing --target $DIR/../data/val.lst --shuffle False --root $DIR/../data/kitti
+python $DIR/prepare_dataset.py --dataset kitti --set training --target $DIR/../data/kitti_train.lst --shuffle True --root $DIR/../data/KITTI
+python $DIR/prepare_dataset.py --dataset kitti --set testing --target $DIR/../data/kitti_val.lst --shuffle False --root $DIR/../data/KITTI
