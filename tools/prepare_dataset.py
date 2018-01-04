63e06442877653de7fcd45afeede8fb7057d43a0
guo.chuanlei <chuanleiguo@gmail.com>
2018-01-04 14:10:14 +0800

    fixed

--- tools/prepare_dataset.py
+++ tools/prepare_dataset.py
@@ -103,9 +103,9 @@ if __name__ == "__main__":
 
     print("List file {} generated...".format(args.target))
 
-    im2rec_path = os.path.join(curr_path, 'tools/im2rec.py')
+    im2rec_path = os.path.join(curr_path, 'im2rec.py')
     if not os.path.exists(im2rec_path):
-        im2rec_path = os.path.join(curr_path, 'tools/im2rec.py')
+        im2rec_path = os.path.join(curr_path, 'im2rec.py')
     subprocess.check_call(["python", im2rec_path,
         os.path.abspath(args.target), os.path.abspath(args.root_path),
         "--shuffle", str(int(args.shuffle)), "--pack-label", "1"])
