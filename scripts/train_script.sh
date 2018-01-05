4b8fb44b76e1f3527c63db3de683765b101303a3
guo.chuanlei <chuanleiguo@gmail.com>
2018-01-05 15:16:37 +0800

    training on GPU by two steps

--- scripts/train_script.sh
+++ scripts/train_script.sh
@@ -5,19 +5,39 @@
 GPUS='1,2,4'
 TRAIN_REC_PATH=./data/train.rec
 VAL_REC_PATH=./data/val.rec
-NETWORK=resnet50
+NETWORK=resnet50-rb
 BATCH_SIZE=32
 DATA_SHAPE=512
 PRETRAINED=./model/ssd_resnet50_512
-OPTIMIZER=adam
-LR=0.0004
+OPTIMIZER=SGD
+LR=0.004
 TENSORBOARD=True
-LR_STEPS=20,40,60
+LR_STEPS=20,40,60,100,200,400,600,800
 FREEZE=""
 ROLLING=True
 ROLLING_TIME=4
-END_EPOCH=1200
-PREFIX="./output/exp2/ssd_"
+END_EPOCH=1000
+PREFIX="./output/exp1/ssd_"
+
+python ./train.py \
+    --train-path ${TRAIN_REC_PATH} \
+    --val-path ${VAL_REC_PATH} \
+    --network ${NETWORK} \
+    --batch-size 128 \
+    --data-shape ${DATA_SHAPE} \
+    --gpus ${GPUS} \
+    --prefix ${PREFIX} \
+    --finetune 1 \
+    --optimizer ${OPTIMIZER} \
+    --tensorboard ${TENSORBOARD} \
+    --monitor 40 \
+    --lr ${LR} \
+    --lr-steps ${LR_STEPS} \
+    --freeze '' \
+    --rolling ${ROLLING} \
+    --rolling_time ${ROLLING_TIME} \
+    --end-epoch 5
+
 
 python ./train.py \
     --train-path ${TRAIN_REC_PATH} \
@@ -27,9 +47,10 @@ python ./train.py \
     --data-shape ${DATA_SHAPE} \
     --gpus ${GPUS} \
     --prefix ${PREFIX} \
-    --resume 100 \
+    --resume 5 \
     --optimizer ${OPTIMIZER} \
     --tensorboard ${TENSORBOARD} \
+    --monitor 40 \
     --lr ${LR} \
     --lr-steps ${LR_STEPS} \
     --freeze '' \
