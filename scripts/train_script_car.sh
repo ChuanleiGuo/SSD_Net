8e68377e14b744c8416fb0192b8dae322e24d409
guo.chuanlei <chuanleiguo@gmail.com>
2018-01-16 14:35:33 +0800

    updated training car model by using res101

--- scripts/train_script_car.sh
+++ scripts/train_script_car.sh
@@ -5,40 +5,41 @@
 GPUS='0,1,2,3'
 TRAIN_REC_PATH=./data/car_train.rec
 VAL_REC_PATH=./data/car_val.rec
-NETWORK=resnet50-rolling
-BATCH_SIZE=32
+NETWORK=resnet101-rolling
+BATCH_SIZE=8
 DATA_SHAPE=2560 768
-PRETRAINED=./model/ssd_resnet50_768
+PRETRAINED=./model/resnet-101
 OPTIMIZER=adam
-LR=0.0001
+LR=0.0004
 TENSORBOARD=True
-LR_STEPS=20,40,100,200,400
-FREEZE=""
+LR_STEPS=20,40,100,200
+FREEZE="^(stage1_|stage2_).*"
+EPOCH=0
 ROLLING=True
 ROLLING_TIME=4
 END_EPOCH=400
-PREFIX="./output/exp1/ssd_"
+PREFIX="./output/exp2/ssd_"
 
-python ./train.py \
-    --train-path ${TRAIN_REC_PATH} \
-    --val-path ${VAL_REC_PATH} \
-    --network ${NETWORK} \
-    --batch-size 128 \
-    --data-shape 768 2560 \
-    --gpus ${GPUS} \
-    --prefix ${PREFIX} \
-    --finetune 1 \
-    --optimizer ${OPTIMIZER} \
-    --tensorboard ${TENSORBOARD} \
-    --lr ${LR} \
-    --lr-steps ${LR_STEPS} \
-    --freeze '' \
-    --num-class 1 \
-    --class-names Car \
-    --overlap 0.7 \
-    --rolling ${ROLLING} \
-    --rolling_time ${ROLLING_TIME} \
-    --end-epoch 100
+# python ./train.py \
+#     --train-path ${TRAIN_REC_PATH} \
+#     --val-path ${VAL_REC_PATH} \
+#     --network ${NETWORK} \
+#     --batch-size 128 \
+#     --data-shape 768 2560 \
+#     --gpus ${GPUS} \
+#     --prefix ${PREFIX} \
+#     --finetune 1 \
+#     --optimizer ${OPTIMIZER} \
+#     --tensorboard ${TENSORBOARD} \
+#     --lr ${LR} \
+#     --lr-steps ${LR_STEPS} \
+#     --freeze '' \
+#     --num-class 1 \
+#     --class-names Car \
+#     --overlap 0.7 \
+#     --rolling ${ROLLING} \
+#     --rolling_time ${ROLLING_TIME} \
+#     --end-epoch 100
 
 
 python ./train.py \
@@ -49,12 +50,13 @@ python ./train.py \
     --data-shape 768 2560 \
     --gpus ${GPUS} \
     --prefix ${PREFIX} \
-    --resume 100 \
+    --pretrained ${PRETRAINED} \
+    --epoch ${EPOCH} \
     --optimizer ${OPTIMIZER} \
     --tensorboard ${TENSORBOARD} \
     --lr ${LR} \
     --lr-steps ${LR_STEPS} \
-    --freeze '' \
+    --freeze ${FREEZE} \
     --num-class 1 \
     --class-names Car \
     --overlap 0.7 \
